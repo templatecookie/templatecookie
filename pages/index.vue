d1539b22c0c04a55112e1286e9ba2a7565711da3
SheikhRashed <devrashed06@gmail.com>
2022-01-08 13:04:57 +0600

    setup: primary complted

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur eius,
+    unde doloribus fugiat aspernatur tenetur delectus doloremque voluptates
+    veritatis voluptatem id! Iusto cupiditate dolorum modi voluptatum
+    perferendis aut numquam dicta?
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
