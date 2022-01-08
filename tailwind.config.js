ef9cb62405e2cbd817722f2faf6cb5bbfede8b04
SheikhRashed <devrashed06@gmail.com>
2022-01-08 16:02:12 +0600

    update: primary, basic colors inlcuded

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "blue-0b": "#0B63E5",
+        "pink-75": "#7534FF",
+        "dark-06": "#061C3D",
+        "dark-42": "#42526B",
+        "gray-f5": "#F5F6F7",
+      },
+      fontFamily: {
+        lexend: ["Lexend", "sans-serif"],
+      },
+      fontSize: {
+        heading: ["72px", "80px"],
+        "heading-40": ["40px", "48px"],
+        title: ["48px", "56px"],
+        body: ["18px", "26px"],
+      },
     },
   },
   variants: {
