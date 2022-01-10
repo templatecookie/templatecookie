64171f828a305d7e458e7874ed5c3bf76d38b6bc
SheikhRashed <devrashed06@gmail.com>
2022-01-10 11:31:25 +0600

    update: business card component replaced on homepage rawcode of service

--- pages/index.vue
+++ pages/index.vue
@@ -71,62 +71,8 @@
         <!-- Project services -->
         <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
           <div v-for="(item, itemIndex) in productList" :key="itemIndex">
-            <!-- service card  -->
-            <div
-              class="bg-white overflow-hidden rounded-2xl relative p-8 flex flex-col items-stretch justify-center text-center"
-            >
-              <!-- icon  -->
-              <div
-                class="inline-block bg-gray-f0 p-5 overflow-hidden rounded-lg w-76 h-76 mx-auto mb-6"
-              >
-                <span>
-                  <svg
-                    width="36"
-                    height="36"
-                    viewBox="0 0 36 36"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      opacity="0.2"
-                      d="M18 31.5C25.4558 31.5 31.5 25.4558 31.5 18C31.5 10.5442 25.4558 4.5 18 4.5C10.5442 4.5 4.5 10.5442 4.5 18C4.5 25.4558 10.5442 31.5 18 31.5Z"
-                      fill="#0B63E5"
-                    />
-                    <path
-                      d="M18 31.5C25.4558 31.5 31.5 25.4558 31.5 18C31.5 10.5442 25.4558 4.5 18 4.5C10.5442 4.5 4.5 10.5442 4.5 18C4.5 25.4558 10.5442 31.5 18 31.5Z"
-                      stroke="#0B63E5"
-                      stroke-width="2.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M16.875 16.875H18V24.75H19.125"
-                      stroke="#0B63E5"
-                      stroke-width="2.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M18.4375 11.8125C18.4375 12.0541 18.2416 12.25 18 12.25C17.7584 12.25 17.5625 12.0541 17.5625 11.8125C17.5625 11.5709 17.7584 11.375 18 11.375C18.2416 11.375 18.4375 11.5709 18.4375 11.8125Z"
-                      fill="#0B63E5"
-                      stroke="#0B63E5"
-                      stroke-width="2.5"
-                    />
-                  </svg>
-                </span>
-              </div>
-
-              <div>
-                <h2
-                  class="text-body-20 font-normal capitalize mb-3 text-dark-06"
-                >
-                  {{ item.title }}
-                </h2>
-                <p class="text-dark-42 text-body-16 font-light">
-                  {{ item.text }}
-                </p>
-              </div>
-            </div>
+            <!-- Service card  -->
+            <BusinessCard :title="item.title" :text="item.text" />
           </div>
         </div>
       </div>
