ece3c9a5f3f5003874d9ad6a0d3257c158a72516
SheikhRashed <devrashed06@gmail.com>
2022-01-10 09:20:16 +0600

    created: plugins folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    // { src: '~/plugins/plugin-name', mode: 'client' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
