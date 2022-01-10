a1196607066db75223114a0a2de4d8657d129662
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-01-10 09:23:52 +0600

    create: site map

---
+++ pages/product-demo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cupiditate explicabo voluptas libero enim excepturi magnam eligendi! Sequi, iure magni optio, facilis mollitia laudantium recusandae exercitationem quaerat adipisci, saepe neque.
+  </div>
+</template>
+<script>
+export default{
+  name: "ProductDemo"
+}
+</script>
