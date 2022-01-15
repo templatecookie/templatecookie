800af8ea44c1e460c583fdde7e07913a0ce53fcd
SheikhRashed <devrashed06@gmail.com>
2022-01-15 12:45:39 +0600

    update: header three created

--- layouts/screenshot.vue
+++ layouts/screenshot.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
+    <HeaderThree />
     <Nuxt />
   </div>
 </template>
 <script>
-export default {};
+import HeaderThree from "../components/Header/HeaderThree.vue";
+export default { components: { HeaderThree } };
 </script>
