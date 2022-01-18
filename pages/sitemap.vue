6ecf7d0b20acda20648376812f14f97b86b7aee1
SheikhRashed <devrashed06@gmail.com>
2022-01-18 17:09:02 +0600

    update: faq remoed

--- pages/sitemap.vue
+++ pages/sitemap.vue
@@ -23,15 +23,6 @@
           >
         </li>
         <li class="mb-1">
-          <nuxt-link to="/faq" class="text-body-16 capitalize text-dark-42"
-            >Faq
-            <span
-              class="text-body-10 rounded leading-none bg-blue-500 text-white p-1"
-              >rashed</span
-            ></nuxt-link
-          >
-        </li>
-        <li class="mb-1">
           <nuxt-link to="/hire-us" class="text-body-16 capitalize text-dark-42"
             >Hire Us
             <span
