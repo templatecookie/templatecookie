ee5848cd4ea45664ae9f467fadb475fa18e932eb
Zakir Hossen <web.zakirbd@gmail.com>
2022-02-07 17:43:42 +0600

    feat: error page added

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "template-cookie",
+    title: 'Homepage | Templatecookie',
     htmlAttrs: {
       lang: "en",
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      },
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
