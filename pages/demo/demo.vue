45949ddd661e7e6a5b1c8eee11787958e4adb021
Zakir Hossen <web.zakirbd@gmail.com>
2022-03-29 21:27:43 +0600

    chore: demo page updated

--- pages/demo/demo.vue
+++ pages/demo/demo.vue
@@ -219,7 +219,7 @@
         <div class="grid grid-cols-12 gap-6">
           <div v-for="(item, itemIndex) in buisnessCard" :key="itemIndex"
             class="col-span-12 xl:col-span-4 lg:col-span-6 md:col-span-6">
-            <div :class="`bg-white rounded-lg p-8 business-card duration-300 hover:shadow-bs8 ${className}`">
+            <div class="bg-white rounded-lg p-8 business-card duration-300 hover:shadow-bs8">
               <div class="icon w-76 h-76 bg-gray-f0 rounded-lg flex items-center justify-center mb-7 duration-300 ">
                 <img src="#" class="w-full h-full object-cover" alt="" />
               </div>
@@ -239,7 +239,7 @@
         <div class="container">
           <div class="lg:max-w-536 m-auto text-center mb-10 lg:mb-16">
             <h2 class="text-dark-06 text-body-32px md:text-4xl lg:text-5xl font-semibold mb-6 tracking-01">
-              Folder Structure
+              ProductFeatureScreenshots - Folder Structure
             </h2>
             <p class="text-dark-06 text-base sm:text-body-18 mb-6 lg:mb-9 font-light font-light">
               Nunc convallis semper justo quis tempor. Praesent molestie, lorem
@@ -280,12 +280,91 @@
         </div>
       </div>
 
-
+      <div class="py-20 lg:py-124">
+        <div class="container">
+          <div class="lg:max-w-536 m-auto text-center mb-10 lg:mb-16">
+            <h3 class="text-blue-0b text-base tracking-ls04 uppercase mb-4">
+              Explore our Gilio team services.
+            </h3>
+            <h2 class="text-dark-06 text-body-32px md:text-4xl lg:text-5xl font-semibold mb-6 tracking-01">
+              DisplayFeatures - Feature Name 01
+            </h2>
+            <p class="text-dark-06 text-base sm:text-body-18 mb-6 lg:mb-9 font-light">
+              Nunc convallis semper justo quis tempor. Praesent molestie, lorem
+              sed imperdiet tempor, libero urna semper urna, facilisis vulputate
+              velit arcu vitae mi. Donec ac nisi ex.
+            </p>
+          </div>
+          <div id="tabs" class="lg:grid grid-cols-12 gap-6">
+            <div class="col-span-8 order-2">
+              <div class="content lg:pl-6 mb-6">
+                <div v-if="activetab === '1'" class="tabcontent">
+                  <div class="h-340 md:h-600">
+                    <img class="w-full h-full object-cover rounded-20 overflow-hidden"
+                      src="~@/assets/images/all-img/one.jpg" alt="" />
+                  </div>
+                </div>
+                <div v-if="activetab === '2'" class="tabcontent">
+                  <div class="h-340 md:h-600">
+                    <img class="w-full h-full object-cover rounded-20 overflow-hidden"
+                      src="~@/assets/images/all-img/two.jpg" alt="" />
+                  </div>
+                </div>
+                <div v-if="activetab === '3'" class="tabcontent">
+                  <div class="h-340 md:h-600">
+                    <img class="w-full h-full object-cover rounded-20 overflow-hidden"
+                      src="~@/assets/images/all-img/three.jpg" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-span-4">
+              <div class="tabs">
+                <div
+                  class="block mb-6 tab-content-wrap cursor-pointer border border-gray-e6 rounded-2xl p-8 duration-300"
+                  v-on:click="activetab = '1'" v-bind:class="[activetab === '1' ? 'active' : '']">
+                  <h3 class="text-dark-06 font-medium text-2xl mb-4 duration-300">
+                    Back-end Development
+                  </h3>
+                  <p class="text-dark-42 test-base font-light duration-300">
+                    Vestibulum ante ipsum primis in faucibus orci luctus et
+                    ultrices posuere cubilia curae; Sed nec diam gravida,
+                    convallis lectus ut.
+                  </p>
+                </div>
+                <div
+                  class="block mb-6 tab-content-wrap cursor-pointer border border-gray-e6 rounded-2xl p-8 duration-300"
+                  v-on:click="activetab = '2'" v-bind:class="[activetab === '2' ? 'active' : '']">
+                  <h3 class="text-dark-06 font-medium text-2xl mb-4 duration-300">
+                    Back-end Development
+                  </h3>
+                  <p class="text-dark-42 test-base font-light duration-300">
+                    Vestibulum ante ipsum primis in faucibus orci luctus et
+                    ultrices posuere cubilia curae; Sed nec diam gravida,
+                    convallis lectus ut.
+                  </p>
+                </div>
+                <div class="block tab-content-wrap cursor-pointer border border-gray-e6 rounded-2xl p-8 duration-300"
+                  v-on:click="activetab = '3'" v-bind:class="[activetab === '3' ? 'active' : '']">
+                  <h3 class="text-dark-06 font-medium text-2xl mb-4 duration-300">
+                    Back-end Development
+                  </h3>
+                  <p class="text-dark-42 test-base font-light duration-300">
+                    Vestibulum ante ipsum primis in faucibus orci luctus et
+                    ultrices posuere cubilia curae; Sed nec diam gravida,
+                    convallis lectus ut.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="py-20 lg:py-124">
         <div class="container">
           <div class="text-center mb-8 lg:mb-12">
             <h2 class="text-dark-06 text-body-32px md:text-4xl lg:text-5xl font-semibold mb-6 tracking-01">
-              45+ Beautiful hand-crafted pages
+              ProductPages - 45+ hand-crafted pages
             </h2>
           </div>
           <div class="grid grid-cols-12 gap-6">
@@ -482,9 +561,18 @@ export default {
       }
     ],
   },
+  methods: {
+    openSidebar() {
+      this.toggleStatus = !this.toggleStatus;
+      this.sideBar = !this.sideBar;
+      document.body.classList.toggle("overlay");
+    },
+  },
   data() {
     return {
-
+      toggleStatus: false,
+      sideBar: false,
+      activetab: '1',
       swiperOptionOne: {
         slidesPerView: 1,
         spaceBetween: 24,
