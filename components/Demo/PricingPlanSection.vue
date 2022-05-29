01653ccdea7ac925ddf85f20b51137f6ee89e8c1
Zakir Hossen <web.zakirbd@gmail.com>
2022-05-29 10:35:36 +0600

    fix: pricing plan color

--- components/Demo/PricingPlanSection.vue
+++ components/Demo/PricingPlanSection.vue
@@ -8,18 +8,19 @@
       </div>
       <div class="flex space-between gap-6 flex-wrap xl:flex-nowrap justify-center">
         <div v-for="(item, index) in data.plans" :key="index" class="w-1/2 rounded-2xl overflow-hidden border-[1px] border-gray-100 bg-white">
-          <div class="border-b-[1px] p-8 border-t-4 border-pink-400 border-b-gray-200">
+          <div class="border-b-[1px] p-8 border-t-4 border-b-gray-200"  :class="item.theme == 'pink' ? 'border-pink-600' : item.theme == 'blue' ? 'border-blue-600' : 'border-green-600'">
             <div class="mb-8 price flex justify-between items-center">
-              <div class="w-20 h-20 rounded-lg bg-pink-200 flex justify-center items-center" :class="item.bgClass">
+              <div class="w-20 h-20 rounded-lg flex justify-center items-center" :class="item.theme == 'pink' ? 'bg-pink-200 ' : item.theme == 'blue' ? 'bg-blue-200' : 'bg-green-200'">
                 <img :src="item.icon.url" class="w-10 h-auto" alt="">
               </div>
-              <h3 class="text-5xl font-semibold text-pink-600"> ${{ item.price }} 
+              <h3 class="text-5xl font-semibold text-pink-600" :class="item.theme == 'pink' ? 'text-pink-600' : item.theme == 'blue' ? 'text-blue-600' : 'text-green-600'"> 
+              ${{ item.price }} 
                 <span class="text-base text-gray-700">/One Time</span>
               </h3>
             </div>
             <h3 class="text-2xl mb-2">{{ item.name }}</h3>
             <p class="font-light"> {{ item.description }} </p>
-            <a :href="item.purchaseLink" to="#" class="mt-8 flex items-center transition-all w-max justify-center text-button-17 rounded-lg overflow-hidden px-8 border-[1px] border-pink-600 hover:text-white hover:bg-pink-600 text-pink-600 bg-transparent">
+            <a :href="item.purchaseLink" to="#" class="mt-8 flex items-center transition-all w-max justify-center text-button-17 rounded-lg overflow-hidden px-8 border-[1px] hover:text-white bg-transparent"  :class="item.theme == 'pink' ? 'border-pink-600 hover:bg-pink-600 text-pink-600' : item.theme == 'blue' ? 'border-blue-600 hover:bg-blue-600 text-blue-600' : 'border-green-600 hover:bg-green-600 text-green-600'">
               Buy Now
               <span class="inline-block ml-3">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
