2329bf86182c55f6d8f1ddd7089824ba25dc5474
Zakir Hossen <33604077+devzakir@users.noreply.github.com>
2022-07-24 14:45:51 +0600

    feat: sitemap support added
    * feat: support added for sitemap
    
    * remove: package-lock json fileL
    
    * feat: sitemap added
    
    * chore: remove dot.env values

---
+++ graphql/allProductListing.js
@@ -0,0 +1,53 @@
+import gql from 'graphql-tag';
+
+// export const global = gql`
+export default gql`
+query products($page: Int!, $pageSize: Int!, $technology: String!, $category: String! ) {
+  products(
+      sort: "publishedAt:desc",
+      pagination: { page: $page, pageSize: $pageSize }
+      filters: {
+        technology: { slug: { contains: $technology } }
+        category: { slug: { contains: $category } }
+      }
+    ) {
+    data {
+      id,
+      attributes {
+        name
+        slug
+        publishedAt
+        short_description
+        regular_price
+        category {
+          data {
+            attributes {
+              name
+              slug
+            }
+          }
+        }
+        demo_banner {
+          data {
+            attributes {
+              name
+              previewUrl
+              url
+              alternativeText
+              ext
+            }
+          }
+        }
+      }
+    }
+    meta {
+      pagination {
+        page
+        pageSize
+        pageCount
+        total
+      }
+    }
+  }
+}
+`
