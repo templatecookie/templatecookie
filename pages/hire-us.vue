b5206ac30eb2902b8fce21af7244d822f109673f
Zakir Hossen <web.zakirbd@gmail.com>
2022-12-20 14:51:01 +0600

    wip: hire us page

--- pages/hire-us.vue
+++ pages/hire-us.vue
@@ -17,7 +17,7 @@
             <div class="lg:grid lg:grid-cols-12 lg:gap-8">
               <div class="px-4 sm:px-6 sm:text-center md:mx-auto md:max-w-2xl lg:col-span-6 lg:flex lg:items-center lg:text-left">
                 <div>
-                  <a href="#" class="inline-flex items-center rounded-full bg-gray-900 p-1 px-3 pr-2 text-white hover:text-gray-200 text-sm">
+                  <a href="#" class="inline-flex items-center rounded-full bg-blue-300 p-1 px-3 pr-2 text-black hover:bg-blue-500 text-sm">
                     Looking for Partner for your Next Web Design & Development Project?
                   </a>
                   <h1 class="mt-4 text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
@@ -44,30 +44,21 @@
                 <div class="bg-white sm:mx-auto sm:w-full sm:max-w-md sm:overflow-hidden sm:rounded-lg">
                   <div class="px-4 py-8 sm:px-10">
                     <p class="text-sm font-medium text-gray-700">Request Free Consultation</p>
-                    <!-- <div class="relative mt-6">
-                      <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                        <div class="w-full border-t border-gray-300"></div>
-                      </div>
-                      <div class="relative flex justify-center text-sm">
-                        <span class="bg-white px-2 text-gray-500">Or</span>
-                      </div>
-                    </div> -->
-
                     <div class="mt-6">
                       <form action="#" method="POST" class="space-y-6">
                         <div>
-                          <label for="name" class="sr-only">Full name</label>
+                          <label for="name" class="sr-only">Your name</label>
                           <input type="text" name="name" id="name" autocomplete="name" placeholder="Full name" required class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                         </div>
 
                         <div>
-                          <label for="mobile-or-email" class="sr-only">Mobile number or email</label>
-                          <input type="text" name="mobile-or-email" id="mobile-or-email" autocomplete="email" placeholder="Mobile number or email" required class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                          <label for="mobile-or-email" class="sr-only">Your email</label>
+                          <input type="text" name="mobile-or-email" id="mobile-or-email" autocomplete="email" placeholder="Your email" required class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                         </div>
 
                         <div>
-                          <label for="password" class="sr-only">Password</label>
-                          <input id="password" name="password" type="password" placeholder="Password" autocomplete="current-password" required class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                          <label for="message" class="sr-only">Message</label>
+                          <textarea id="message" placeholder="Enter your message" name="message" rows="4" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"></textarea>
                         </div>
 
                         <div>
@@ -97,13 +88,12 @@
                 <div class="-mt-6">
                   <div>
                     <span class="inline-flex items-center justify-center rounded-xl bg-indigo-500 p-3 shadow-lg">
-                      <!-- Heroicon name: outline/cloud-arrow-up -->
                       <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                       </svg>
                     </span>
                   </div>
-                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Push to Deploy</h3>
+                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Product Design (UI/UX)</h3>
                   <p class="mt-5 text-base leading-7 text-gray-600">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
                 </div>
               </div>
@@ -114,13 +104,12 @@
                 <div class="-mt-6">
                   <div>
                     <span class="inline-flex items-center justify-center rounded-xl bg-indigo-500 p-3 shadow-lg">
-                      <!-- Heroicon name: outline/lock-closed -->
                       <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                       </svg>
                     </span>
                   </div>
-                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">SSL Certificates</h3>
+                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Custom Software Development</h3>
                   <p class="mt-5 text-base leading-7 text-gray-600">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
                 </div>
               </div>
@@ -131,13 +120,12 @@
                 <div class="-mt-6">
                   <div>
                     <span class="inline-flex items-center justify-center rounded-xl bg-indigo-500 p-3 shadow-lg">
-                      <!-- Heroicon name: outline/arrow-path -->
                       <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12c0-1.232.046-2.453.138-3.662a4.006 4.006 0 013.7-3.7 48.678 48.678 0 017.324 0 4.006 4.006 0 013.7 3.7c.017.22.032.441.046.662M4.5 12l-3-3m3 3l3-3m12 3c0 1.232-.046 2.453-.138 3.662a4.006 4.006 0 01-3.7 3.7 48.657 48.657 0 01-7.324 0 4.006 4.006 0 01-3.7-3.7c-.017-.22-.032-.441-.046-.662M19.5 12l-3 3m3-3l3 3" />
                       </svg>
                     </span>
                   </div>
-                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Simple Queues</h3>
+                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Online Marketplace Development</h3>
                   <p class="mt-5 text-base leading-7 text-gray-600">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
                 </div>
               </div>
@@ -148,13 +136,12 @@
                 <div class="-mt-6">
                   <div>
                     <span class="inline-flex items-center justify-center rounded-xl bg-indigo-500 p-3 shadow-lg">
-                      <!-- Heroicon name: outline/shield-check -->
                       <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                       </svg>
                     </span>
                   </div>
-                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Advanced Security</h3>
+                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">MVP Development</h3>
                   <p class="mt-5 text-base leading-7 text-gray-600">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
                 </div>
               </div>
@@ -165,30 +152,12 @@
                 <div class="-mt-6">
                   <div>
                     <span class="inline-flex items-center justify-center rounded-xl bg-indigo-500 p-3 shadow-lg">
-                      <!-- Heroicon name: outline/cog -->
                       <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" />
                       </svg>
                     </span>
                   </div>
-                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Powerful API</h3>
-                  <p class="mt-5 text-base leading-7 text-gray-600">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
-                </div>
-              </div>
-            </div>
-
-            <div class="pt-6">
-              <div class="flow-root rounded-lg bg-gray-50 px-6 pb-8">
-                <div class="-mt-6">
-                  <div>
-                    <span class="inline-flex items-center justify-center rounded-xl bg-indigo-500 p-3 shadow-lg">
-                      <!-- Heroicon name: outline/server -->
-                      <svg class="h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 17.25v-.228a4.5 4.5 0 00-.12-1.03l-2.268-9.64a3.375 3.375 0 00-3.285-2.602H7.923a3.375 3.375 0 00-3.285 2.602l-2.268 9.64a4.5 4.5 0 00-.12 1.03v.228m19.5 0a3 3 0 01-3 3H5.25a3 3 0 01-3-3m19.5 0a3 3 0 00-3-3H5.25a3 3 0 00-3 3m16.5 0h.008v.008h-.008v-.008zm-3 0h.008v.008h-.008v-.008z" />
-                      </svg>
-                    </span>
-                  </div>
-                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">Database Backups</h3>
+                  <h3 class="mt-8 text-lg font-semibold leading-8 tracking-tight text-gray-900">SAAS Development</h3>
                   <p class="mt-5 text-base leading-7 text-gray-600">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</p>
                 </div>
               </div>
@@ -201,12 +170,12 @@
     <!-- HTML -->
       <section class="bg-gray-50 ">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:py-16 lg:px-8">
-          <div class="mb-20">
+          <div class="mb-20 text-center">
             <h2 class="text-lg font-semibold text-indigo-600">Deploy faster</h2>
             <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               Our working process
             </p>
-            <p class="mt-5 max-w-prose text-xl text-gray-500">Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.</p>
+            <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.</p>
           </div>
           <div class="grid grid-cols-12 gap-6">
             <div v-for="(item, itemIndex) in featureList" :key="itemIndex"
@@ -227,38 +196,45 @@
 
       <div class="bg-white">
         <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-          <div class="lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
-            <div>
+          <div class="">
+            <div class="mb-8 text-center">
               <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                 Technology We Use
               </h2>
-              <p class="mt-3 max-w-3xl text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue.</p>
-              <div class="mt-8 sm:flex">
-                <div class="rounded-md shadow">
-                  <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700">Create Account</a>
-                </div>
-                <div class="mt-3 sm:mt-0 sm:ml-3">
-                  <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-100 px-5 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200">Contact Us</a>
-                </div>
-              </div>
+              <p class="mt-3 mx-auto max-w-3xl text-lg text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue.</p>
             </div>
-            <div class="mt-8 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
+
+            <div class="mt-8 grid grid-cols-3 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-4">
               <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">Nuxtjs</div>
                 <img class="max-h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Workcation">
               </div>
               <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">Vuejs</div>
                 <img class="max-h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage">
               </div>
               <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">Tailwind CSS</div>
                 <img class="max-h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
               </div>
               <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">Laravel</div>
+                <img class="max-h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
+              </div>
+              <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">MySQL</div>
                 <img class="max-h-12" src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg" alt="Laravel">
               </div>
               <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">Dato CMS</div>
                 <img class="max-h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
               </div>
               <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">Digitalocean</div>
+                <img class="max-h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
+              </div>
+              <div class="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
+                <div class="sr-only">Vercel</div>
                 <img class="max-h-12" src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg" alt="Statamic">
               </div>
             </div>
@@ -294,42 +270,16 @@
           </div>
           <div class="mt-12">
             <form action="#" method="POST" class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
-              <div>
-                <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
+              <div class="sm:col-span-2">
+                <label for="first-name" class="block text-sm font-medium text-gray-700">Your name</label>
                 <div class="mt-1">
                   <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
                 </div>
               </div>
-              <div>
-                <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
-                <div class="mt-1">
-                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-                </div>
-              </div>
               <div class="sm:col-span-2">
-                <label for="company" class="block text-sm font-medium text-gray-700">Company</label>
-                <div class="mt-1">
-                  <input type="text" name="company" id="company" autocomplete="organization" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-                </div>
-              </div>
-              <div class="sm:col-span-2">
-                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                <label for="email" class="block text-sm font-medium text-gray-700">Your e mail</label>
                 <div class="mt-1">
                   <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
-                </div>
-              </div>
-              <div class="sm:col-span-2">
-                <label for="phone-number" class="block text-sm font-medium text-gray-700">Phone Number</label>
-                <div class="relative mt-1 rounded-md shadow-sm">
-                  <div class="absolute inset-y-0 left-0 flex items-center">
-                    <label for="country" class="sr-only">Country</label>
-                    <select id="country" name="country" class="h-full rounded-md border-transparent bg-transparent py-0 pl-4 pr-8 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500">
-                      <option>US</option>
-                      <option>CA</option>
-                      <option>EU</option>
-                    </select>
-                  </div>
-                  <input type="text" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-gray-300 py-3 px-4 pl-20 focus:border-indigo-500 focus:ring-indigo-500" placeholder="+1 (555) 987-6543">
                 </div>
               </div>
               <div class="sm:col-span-2">
