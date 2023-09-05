e43f1af770308ae369569a98e753197b2c780d82
Rakib0101 <rakib.01jan@gmail.com>
2023-09-05 12:53:48 +0600

    chore: fix scroll and url updated issues

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,160 +1,78 @@
-<!-- <template>
-    <div>
-        <ul class="flex justify-center items-center space-x-4">
-            <li>
-                <a href="#" class="bg-gray-200 text-gray-600 flex items-center text-sm font-medium capitalize rounded-md p-3 sm:px-5">
-                    <span class="icon mr-2 inline-block">
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M16.875 10H3.125" stroke="#525252" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M8.75 4.375L3.125 10L8.75 15.625" stroke="#525252" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </span>
-                    Prev
-                </a>
-                <a href="#" class="bg-gray-f0 text-primary flex items-center text-sm font-medium capitalize rounded-md p-3 sm:px-5">
-                    <span class="icon mr-2 inline-block">
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M16.875 10H3.125" stroke="#0B63E5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M8.75 4.375L3.125 10L8.75 15.625" stroke="#0B63E5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </span>
-                    Prev
-                </a>
-            </li>
-            <li>
-                <ul class="flex items-center max-w-100 sm:max-w-full overflow-hidden overflow-x-scroll sm:overflow-x-hidden">
-                    <li>
-                        <a href="#" class="inline-block text-sm text-dark-gray hover:text-primary font-normal px-3.5 p-3 overflow-hidden rounded-md hover:bg-gray-f0">
-                            1
-                        </a>
-                        <a class="active inline-block text-sm text-dark-gray hover:text-primary font-normal px-3.5 p-3 overflow-hidden rounded-md hover:bg-gray-f0">
-                            2
-                        </a>
-                        <a href="#" class="inline-block text-sm text-dark-gray hover:text-primary font-normal px-3.5 p-3 overflow-hidden rounded-md hover:bg-gray-f0">
-                            3
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#" class="bg-gray-200 text-gray-600 flex items-center text-sm font-medium capitalize rounded-md p-2.5 sm:px-5">
-                    Next
-                    <span class="icon ml-2 inline-block">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3.75 12H20.25" stroke="#525252" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="#525252" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </span>
-                </a>
-                <a href="#" class="bg-gray-f0 text-primary flex items-center text-sm font-medium capitalize rounded-md p-2.5 sm:px-5">
-                    Next
-                    <span class="icon ml-2 inline-block">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3.75 12H20.25" stroke="#0B63E5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="#0B63E5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </span>
-                </a>
-            </li>
-        </ul>
-    </div>
-</template>
-
-<script>
-    export default {
-        name: "Pagination",
-        props: ["data"],
-        methods: {
-            prevDisabled() {
-                return this.data.page < 2;
-            },
-            nextDisabled() {
-                return this.data.pageCount == this.data.page;
-            },
-            leadingZero(value) {
-                return value < 10 ? "0" + value : value;
-            },
-            goToPage(page) {
-                this.$emit("goToPage", page);
-            },
-            goToPreviousPage() {
-                const currentPage = this.data.page;
-                this.$emit("goToPage", currentPage - 1);
-            },
-            goToNextPage() {
-                const currentPage = this.data.page;
-                this.$emit("goToPage", currentPage + 1);
-            },
-        },
-    };
-</script>
-<style scoped>
-    .active {
-        color: #fff;
-        background-color: #0b63e5;
-    }
-</style> -->
 <template>
-    <ul class="flex justify-center items-center space-x-4">
-        <li>
-            <a @click.prevent="prevPage" :class="currentPage === 1 ? 'bg-gray-200 text-gray-600 cursor-not-allowed':'bg-gray-f0 text-primary cursor-pointer'" class="flex items-center text-sm font-medium capitalize rounded-md p-3 sm:px-5">
-                <span class="icon mr-2 inline-block">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M16.875 10H3.125" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M8.75 4.375L3.125 10L8.75 15.625" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </span>
-                Prev
-            </a>
-        </li>
-        <!-- Display page numbers here -->
-        <li v-for="page in totalPages" :key="page" class="pagination-list flex justify-center items-center">
-            <a @click.prevent="goToPage(page)" :class="{ 'active': currentPage === page }" class="cursor-pointer inline-block text-sm text-dark-gray hover:text-primary font-normal px-3.5 p-3 overflow-hidden rounded-md hover:bg-gray-f0">
-                {{ page }}
-            </a>
-        </li>
-        <li>
-            <a @click.prevent="nextPage" :class="currentPage === totalPages ? 'bg-gray-200 text-gray-600 cursor-not-allowed':'bg-gray-f0 text-primary cursor-pointer'" class="flex items-center text-sm font-medium capitalize rounded-md p-3 sm:px-5">
-                Next
-                <span class="icon ml-2 inline-block">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M3.75 12H20.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </span>
-            </a>
-        </li>
-    </ul>
+  <ul class="flex justify-center items-center space-x-4">
+    <li>
+      <a @click.prevent="prevPage"
+        :class="currentPage === 1 ? 'bg-gray-200 text-gray-600 cursor-not-allowed':'bg-gray-f0 text-primary cursor-pointer'"
+        class="flex items-center text-sm font-medium capitalize rounded-md p-3 sm:px-5">
+        <span class="icon mr-2 inline-block">
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.875 10H3.125" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+              stroke-linejoin="round" />
+            <path d="M8.75 4.375L3.125 10L8.75 15.625" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+              stroke-linejoin="round" />
+          </svg>
+        </span>
+        Prev
+      </a>
+    </li>
+    <li v-for="page in totalPages" :key="page" class="pagination-list flex justify-center items-center">
+      <a @click.prevent="goToPage(page)" :class="{ 'active': currentPage === page }"
+        class="cursor-pointer inline-block text-sm text-dark-gray hover:text-primary font-normal px-3.5 p-3 overflow-hidden rounded-md hover:bg-gray-f0">
+        {{ page }}
+      </a>
+    </li>
+    <li>
+      <a @click.prevent="nextPage"
+        :class="currentPage === totalPages ? 'bg-gray-200 text-gray-600 cursor-not-allowed':'bg-gray-f0 text-primary cursor-pointer'"
+        class="flex items-center text-sm font-medium capitalize rounded-md p-3 sm:px-5">
+        Next
+        <span class="icon ml-2 inline-block">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.75 12H20.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+              stroke-linejoin="round" />
+            <path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+              stroke-linejoin="round" />
+          </svg>
+        </span>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
-    export default {
-        props: {
-            currentPage: Number,
-            totalPages: Number,
-        },
-        methods: {
-            prevPage() {
-                if (this.currentPage > 1) {
-                    this.$emit('page-change', this.currentPage - 1);
-                }
-            },
-            nextPage() {
-                if (this.currentPage < this.totalPages) {
-                    this.$emit('page-change', this.currentPage + 1);
-                }
-            },
-            goToPage(page) {
-                if (this.currentPage !== page) {
-                    this.$emit('page-change', page);
-                }
-            },
-        },
-    };
+  export default {
+    props: {
+      currentPage: Number,
+      totalPages: Number,
+    },
+    methods: {
+      prevPage() {
+        if (this.currentPage > 1) {
+          this.$router.push({ query: { page: this.currentPage - 1 } });
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+          this.$emit('page-change', this.currentPage - 1);
+        }
+      },
+      nextPage() {
+        if (this.currentPage < this.totalPages) {
+          this.$router.push({ query: { page: this.currentPage + 1 } });
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+          this.$emit('page-change', this.currentPage + 1);
+        }
+      },
+      goToPage(page) {
+        if (this.currentPage !== page) {
+          this.$router.push({ query: { page } });
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+          this.$emit('page-change', page);
+        }
+      },
+    },
+  };
 </script>
 
 <style scoped>
-  .pagination-list .active{
+  .pagination-list .active {
     background: rgba(11, 99, 229, 1);
     color: white;
   }
