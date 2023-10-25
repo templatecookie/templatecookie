ef66c0b77d101bba2d16737372828d3598abbb80
Rakib0101 <rakib.01jan@gmail.com>
2023-10-25 11:01:14 +0600

    style: infusionsoft form added and removed hubspot form from hire us page

--- components/hire/HeroSection.vue
+++ components/hire/HeroSection.vue
@@ -1,80 +1,101 @@
 <template>
   <div class="relative overflow-hidden bg-gray-800">
-    <div
-      class="hidden sm:absolute sm:inset-0 sm:block"
-      aria-hidden="true"
-    >
+    <div class="hidden sm:absolute sm:right-0 sm:top-12 sm:inline-block" aria-hidden="true">
       <svg
         class="absolute bottom-0 right-0 mb-48 translate-x-1/2 transform text-gray-700 lg:top-0 lg:mt-28 lg:mb-0 xl:translate-x-0 xl:transform-none"
-        width="364"
-        height="384"
-        viewBox="0 0 364 384"
-        fill="none"
-      >
+        width="364" height="384" viewBox="0 0 364 384" fill="none">
         <defs>
-          <pattern
-            id="eab71dd9-9d7a-47bd-8044-256344ee00d0"
-            x="0"
-            y="0"
-            width="20"
-            height="20"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="4"
-              height="4"
-              fill="currentColor"
-            />
+          <pattern id="eab71dd9-9d7a-47bd-8044-256344ee00d0" x="0" y="0" width="20" height="20"
+            patternUnits="userSpaceOnUse">
+            <rect x="0" y="0" width="4" height="4" fill="currentColor" />
           </pattern>
         </defs>
-        <rect
-          width="364"
-          height="384"
-          fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)"
-        />
+        <rect width="364" height="384" fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)" />
       </svg>
     </div>
     <div class="mx-auto max-w-7xl py-20 sm:py-24 lg:py-32 px-4 sm:px-6 lg:px-8">
       <div class="lg:grid lg:grid-cols-6 lg:gap-8">
-        <div
-          class="px-4 mb-6 sm:px-6 sm:text-center md:mx-auto lg:col-span-6 lg:flex lg:items-center lg:text-center"
-        >
+        <div class="px-4 mb-6 sm:px-6 sm:text-center md:mx-auto lg:col-span-6 lg:flex lg:items-center lg:text-center">
           <div>
-            <a
-              href="#"
+            <a href="#"
               class="inline-flex items-center rounded-full bg-blue-300 p-1 px-3 pr-2 text-black hover:bg-blue-500 text-sm"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
+              data-aos="fade-up" data-aos-delay="100">
               Partner with us today!
             </a>
-            <h1
-              data-aos="fade-up"
-              class="mt-4 text-4xl font-bold text-white sm:text-5xl lg:text-6xl lg:leading-snug"
-              data-aos-delay="200"
-            >
+            <h1 data-aos="fade-up" class="mt-4 text-4xl font-bold text-white sm:text-5xl lg:text-6xl lg:leading-snug"
+              data-aos-delay="200">
               Looking for Partner <br>
               for your Next Project?
             </h1>
-            <p
-              data-aos="fade-up"
-              data-aos-delay="300"
-              class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg md:max-w-2xl mx-auto"
-            >
+            <p data-aos="fade-up" data-aos-delay="300"
+              class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg md:max-w-2xl mx-auto">
               Templatecookie have a talented and experienced team of designers
               and developers who are committed to offering the best value to
               your projects.
             </p>
           </div>
         </div>
-        <div class="mt-16 sm:mt-24 lg:col-span-6 lg:mt-0">
+        <!-- <div class="mt-16 sm:mt-24 lg:col-span-6 lg:mt-0">
           <div
             id="hubspotForm"
             class="px-6 pb-4 pt-8 bg-white sm:mx-auto sm:w-full sm:max-w-lg sm:overflow-hidden sm:rounded-lg"
             data-aos="zoom-in"
           />
+        </div> -->
+      </div>
+      <div class="w-full mt-12 mx-auto md:w-2/5">
+        <div
+          class="relative z-10 h-auto p-8 py-10 overflow-hidden bg-white border-b-2 border-gray-300 rounded-lg shadow-2xl px-7"
+          data-rounded="rounded-lg" data-rounded-max="rounded-full">
+          <h3 class="mb-6 text-2xl font-medium text-center">Hire Us</h3>
+          <form accept-charset="UTF-8"
+            action="https://pbj887.infusionsoft.com/app/form/process/a686c8c396862f4b1d3fd2e84ff2f89f"
+            class="infusion-form" id="inf_form_a686c8c396862f4b1d3fd2e84ff2f89f" method="POST">
+            <input name="inf_form_xid" type="hidden" value="a686c8c396862f4b1d3fd2e84ff2f89f" />
+            <input name="inf_form_name" type="hidden" value="hire-us form submitted" />
+            <input name="infusionsoft_version" type="hidden" value="1.70.0.599909" />
+            <div class="flex flex-col gap-4">
+              <div class="flex gap-5 items-center">
+                <div class="infusion-field w-full">
+                  <label for="inf_field_FirstName" class="flex mb-1.5 text-sm">First Name <span class="text-sx text-red-600">*</span></label>
+                  <input id="inf_field_FirstName" name="inf_field_FirstName" class="transition-all duration-300 rounded-md hover:bg-gray-50 focus:bg-gray-50" placeholder="First Name" type="text" />
+                </div>
+                <div class="infusion-field w-full">
+                  <label for="inf_field_LastName" class="flex mb-1.5 text-sm">Last Name <span class="text-sx text-red-600">*</span></label>
+                  <input id="inf_field_LastName" name="inf_field_LastName" placeholder="Last Name" class="transition-all duration-300 rounded-md hover:bg-gray-50 focus:bg-gray-50" type="text" />
+                </div>
+              </div>
+              <div class="flex gap-5 items-center">
+                <div class="infusion-field w-full">
+                  <label for="inf_field_Email" class="flex mb-1.5 text-sm">Email <span class="text-sx text-red-600">*</span></label>
+                  <input id="inf_field_Email" name="inf_field_Email" placeholder="Email" class="transition-all duration-300 w-full rounded-md hover:bg-gray-50 focus:bg-gray-50" type="text" />
+                </div>
+                <div class="infusion-field w-full">
+                  <label for="inf_custom_Chooseyourbudget" class="flex mb-1.5 text-sm">Choose your budget <span class="text-sx text-red-600">*</span></label>
+                  <div class="infusion-field-input-container flex">
+                    <select id="inf_custom_Chooseyourbudget" class="transition-all duration-300 w-full rounded-md hover:bg-gray-50 focus:bg-gray-50" name="inf_custom_Chooseyourbudget">
+                      <option selected="selected" value="$2000 to $4000">$2000 to $4000</option>
+                      <option value="$1000 to $2000">$1000 to $2000</option>
+                      <option value="$500 to $1000">$500 to $1000</option>
+                      <option value="Below $500">Below $500</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="infusion-field w-full">
+                <label for="inf_custom_Writemessage" class="flex mb-1.5 text-sm">Write message <span class="text-sx text-red-600">*</span></label>
+                <textarea id="inf_custom_Writemessage" name="inf_custom_Writemessage"
+                  placeholder="Write message" rows="5" class="transition-all duration-300 flex w-full rounded-md hover:bg-gray-50 focus:bg-gray-50"></textarea>
+              </div>
+              <div class="infusion-submit">
+                <button class="infusion-recaptcha bg-primary hover:bg-secondary text-sm text-white rounded-md px-4 py-2" id="recaptcha_a686c8c396862f4b1d3fd2e84ff2f89f"
+                  type="submit">Submit</button>
+              </div>
+              <div class="hidden">
+                <div>&nbsp;</div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -82,14 +103,14 @@
 </template>
 
 <script>
-export default {
-  mounted() {
-    window.hbspt.forms.create({
-      region: "na1",
-      portalId: "22174697",
-      formId: "55ae1706-e17a-40a0-81db-275fc2d1030a",
-      target: "#hubspotForm",
-    });
-  },
-};
+  export default {
+    mounted() {
+      window.hbspt.forms.create({
+        region: "na1",
+        portalId: "22174697",
+        formId: "55ae1706-e17a-40a0-81db-275fc2d1030a",
+        target: "#hubspotForm",
+      });
+    },
+  };
 </script>
--- pages/hire-us.vue
+++ pages/hire-us.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <hire-hero-section v-if="hubspotLoaded" />
+    <hire-hero-section />
     <hire-our-services />
     <hire-working-process />
     <hire-our-experties />
@@ -29,6 +29,14 @@
             rel: 'canonical',
             href: 'https://www.templatecookie.com' + route.path,
           },
+        ],
+        script: [
+          { type:"text/javascript", src: "https://pbj887.infusionsoft.app/app/webTracking/getTrackingCode" },
+          { type:"text/javascript", src: "https://pbj887.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.599909" },
+          { src: "https://www.google.com/recaptcha/api.js?onload=onloadInfusionRecaptchaCallback&render=explicit", async:"async", defer:'defer' },
+          { type:"text/javascript", src: "https://pbj887.infusionsoft.com/app/timezone/timezoneInputJs?xid=a686c8c396862f4b1d3fd2e84ff2f89f" },
+          { type:"text/javascript", src: "https://pbj887.infusionsoft.com/js/jquery/jquery-3.3.1.js" },
+          { type:"text/javascript", src: "https://pbj887.infusionsoft.app/app/webform/overwriteRefererJs" },
         ],
       })
     },
