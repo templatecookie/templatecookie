81b44229d905add3249312cd87b322748e7e27f7
shuvo-me <shuvo.js.dev@gmail.com>
2023-10-30 17:17:54 +0600

    fix heading and description is not updating when link change in product documentation section.

--- pages/docs/[slug].vue
+++ pages/docs/[slug].vue
@@ -2,15 +2,14 @@
   <div>
     <section
       class="py-12 lg:pb-12 bg-no-repeat bg-center bg-cover"
-      style="background-image: url(&quot;/hero-bg.png&quot;)"
+      style="background-image: url('/hero-bg.png')"
     >
       <div class="mx-auto max-w-7xl px-4 sm:px-6">
         <div class="text-left">
           <h1
             class="text-4xl md:text-heading-40 text-dark mb-6 mx-auto font-semibold capitalize"
           >
-            <span v-if="product?.title">{{ product.title }}</span>
-            - Documentation
+            <span v-if="product?.title" v-text="product.title" />
           </h1>
           <p
             class="text-lg md:text-lg text-dark mb-8 font-light"
@@ -33,7 +32,7 @@
                   :href="item?._path"
                   class="px-4 py-2 inline-block w-full text-gray-700 font-light text-sm"
                 >
-                  {{ item.title }}
+                  {{ item.link }}
                 </nuxt-link>
               </li>
             </ul>
@@ -68,25 +67,11 @@
 </template>
 
 <script setup>
-const { path } = useRoute();
-const newPath = path.match(/^\/docs\/\w+/);
-// fetch all pages.
-const { data } = await useAsyncData("docs-product", () =>
-  queryContent(`${newPath[0]}`)
-    .only(["title", "description", "category", "position", "_path", "_dir"])
-    .sort({ position: 1 })
-    .find(),
-);
-
-// find index.md and create product short name from _path
-const product = ref([]);
-product.value = data._rawValue.shift();
-const productName = product?._path?.replace("/docs/", "");
-
-// group by categories using lodash library
 import groupBy from "lodash.groupby";
-const categories = groupBy(data._rawValue, "category");
+
 const route = useRoute();
+const router = useRouter();
+
 useHead({
   link: [
     {
@@ -95,6 +80,63 @@ useHead({
     },
   ],
 });
+
+const newPath = route.path.match(/^\/docs\/\w+/);
+// fetch all pages.
+const { data } = await useAsyncData("docs-product", () =>
+  queryContent(`${newPath[0]}`)
+    .only(["title", "description", "category", "position", "_path", "_dir"])
+    .sort({ position: 1 })
+    .find()
+);
+
+const generateDocTitle = (str = "") => {
+  let title;
+  if (route.fullPath.includes("/docs/adlisting")) {
+    title = str.toLowerCase().includes("adlisting")
+      ? `${str} - Documentation`
+      : `${str} - Adlisting Documentation`;
+  }
+  if (route.fullPath.includes("/docs/jobpilot")) {
+    title = str.toLowerCase().includes("jobpilot") ? str : `Jobpilot - ${str}`;
+  }
+  if (route.fullPath.includes("/docs/schooling")) {
+    title = str.toLowerCase().includes("schooling")
+      ? str
+      : `Schooling - ${str}`;
+  }
+  console.log({ str, title });
+  return title;
+};
+
+// find index.md and create product short name from _path
+const product = ref([]);
+product.value = data._rawValue.find((item) => {
+  if (item._path === route.path.replace(/\/+$/, "")) {
+    item.title = generateDocTitle(item.title);
+    console.log({ item });
+    return item;
+  }
+});
+
+router.afterEach((updatedRoute) => {
+  console.log({ data, updatedRoute });
+  product.value = data._rawValue.find((item) => {
+    if (item._path === updatedRoute.path) {
+      item.title = generateDocTitle(item.title);
+      console.log({ item });
+      return item;
+    }
+  });
+  console.log({ tt: toRaw(product) });
+});
+
+// group by categories using lodash library
+
+const categories = groupBy(
+  data._rawValue.map((item) => ({ ...item, link: item.title })),
+  "category"
+);
 </script>
 
 <style scoped>
