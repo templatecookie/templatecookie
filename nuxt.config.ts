b6459d2c6774e7cc2b43aeaadbfe343fb20022a8
shuvo-me <shuvo.js.dev@gmail.com>
2023-11-01 11:49:55 +0600

    fix meta description duplicate issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,6 @@ export default defineNuxtConfig({
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
-          hid: "description",
           name: "description",
           content:
             "A team of talented developers working on building quality UI,HTML templates and PHP scripts! Reuseable Website Templates & PHP Scripts for your next project!",
