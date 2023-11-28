0fc45d825a2901ee838245abc84b44f717895be0
shuvo-me <shuvo.js.dev@gmail.com>
2023-11-28 11:48:06 +0600

    added: process section

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="space-y-[4rem]">
     <!-- Hero -->
     <div class="relative overflow-hidden">
       <!-- Gradients -->
@@ -121,6 +121,402 @@
       </div>
     </div>
     <!-- End Hero -->
+
+    <!-- Service  -->
+    <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      <div class="text-center">
+        <h2
+          class="text-3xl text-gray-800 font-bold lg:text-4xl dark:text-white"
+        >
+          Our Custom Web Development Services Can Help You With:
+        </h2>
+        <p class="mt-3 text-gray-800 dark:text-gray-400">
+          "With years of experience in providing custom web development
+          services, we apply industry best practices to achieve superior
+          results."
+        </p>
+      </div>
+      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div class="grid grid-cols-1 sm:grid-cols-2 items-center gap-12">
+          <div>
+            <div class="flex items-center gap-4">
+              <svg
+                class="flex-shrink-0 h-8 w-8 text-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
+              </svg>
+
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                Striking Aesthetic Appeal
+              </h3>
+            </div>
+
+            <div
+              class="bg-gradient-to-r from-gray-200 via-gray-50 to-white/0 h-0.5 mt-6"
+            >
+              <div class="bg-gray-400 w-9 h-0.5"></div>
+            </div>
+            <div class="mt-5">
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                The appearance of your website can make all the difference for
+                customers. That's why our designs are not just eye-catching but
+                also intuitive, that will help boost the conversion rate.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div class="flex items-center gap-4">
+              <svg
+                class="flex-shrink-0 h-8 w-8 text-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
+              </svg>
+
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                Full-cycle Web Development Services
+              </h3>
+            </div>
+
+            <div
+              class="bg-gradient-to-r from-gray-200 via-gray-50 to-white/0 h-0.5 mt-6"
+            >
+              <div class="bg-gray-400 w-9 h-0.5"></div>
+            </div>
+            <div class="mt-5">
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                We're here to support you every step of the way in your web
+                development journey, offering professional assistance at every
+                point of contact with our customers.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div class="flex items-center gap-4">
+              <svg
+                class="flex-shrink-0 h-8 w-8 text-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
+              </svg>
+
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                Robust Tech Stack
+              </h3>
+            </div>
+
+            <div
+              class="bg-gradient-to-r from-gray-200 via-gray-50 to-white/0 h-0.5 mt-6"
+            >
+              <div class="bg-gray-400 w-9 h-0.5"></div>
+            </div>
+            <div class="mt-5">
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                We stay on top of the game in custom web development by
+                continually upgrading our expertise with the latest
+                technologies. This ensures we deliver future-oriented web
+                solutions to our clients.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div class="flex items-center gap-4">
+              <svg
+                class="flex-shrink-0 h-8 w-8 text-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
+              </svg>
+
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                Upgraded Ideas
+              </h3>
+            </div>
+
+            <div
+              class="bg-gradient-to-r from-gray-200 via-gray-50 to-white/0 h-0.5 mt-6"
+            >
+              <div class="bg-gray-400 w-9 h-0.5"></div>
+            </div>
+            <div class="mt-5">
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                Pairing your business goals with our custom web development
+                services gives them a whole new level of strength and impact.
+                Our solid tech foundation transforms your objectives into a
+                powerful reality.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div class="flex items-center gap-4">
+              <svg
+                class="flex-shrink-0 h-8 w-8 text-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2" />
+              </svg>
+
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
+                Wise Investment
+              </h3>
+            </div>
+
+            <div
+              class="bg-gradient-to-r from-gray-200 via-gray-50 to-white/0 h-0.5 mt-6"
+            >
+              <div class="bg-gray-400 w-9 h-0.5"></div>
+            </div>
+            <div class="mt-5">
+              <p class="mt-1 text-gray-600 dark:text-gray-400">
+                Experience the joy of web development with TemplateCookie: our
+                budget-friendly approach ensures you won't break the bank,
+                turning the process into a true pleasure without overspending.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Service  -->
+
+    <!-- Process -->
+    <div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto space-y-[2rem]">
+      <div class="text-left">
+        <h2 class="text-3xl text-gray-800 font-bold lg:text-4xl">
+          Our Custom Web Development Services Can Help You With:
+        </h2>
+        <p class="mt-3 text-gray-800">
+          "With years of experience in providing custom web development
+          services, we apply industry best practices to achieve superior
+          results."
+        </p>
+      </div>
+      <ol class="relative border-s border-gray-200">
+        <li class="mb-10 ms-8">
+          <span
+            class="absolute flex items-center justify-center w-[45px] h-[45px] bg-blue-100 text-blue-700 rounded-full -start-6 ring-8 ring-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+              />
+            </svg>
+          </span>
+          <h3
+            class="flex items-center mb-1 text-lg font-semibold text-gray-900"
+          >
+            Consulting, Planning, Research Analysis and Everything in Between
+          </h3>
+
+          <p class="mb-4 text-base font-normal text-gray-500">
+            We kick off our development journey with a client interview to truly
+            grasp their needs. Our expert team then charts out a detailed
+            schedule, crafts a detailed plan outlining the solution's scope and
+            identifying costs. We're all about efficiency, so we explore
+            cost-cutting opportunities too. Additionally, our custom web
+            development services delve deep into competitive analysis and target
+            audience research. Once we agree on the website's key priorities,
+            it's full steam ahead to the next stage of development.
+          </p>
+        </li>
+        <li class="mb-10 ms-8">
+          <span
+            class="absolute flex items-center justify-center w-[45px] h-[45px] bg-blue-100 text-blue-700 rounded-full -start-6 ring-8 ring-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+              />
+            </svg>
+          </span>
+          <h3
+            class="flex items-center mb-1 text-lg font-semibold text-gray-900"
+          >
+            Design and Prototyping
+          </h3>
+
+          <p class="mb-4 text-base font-normal text-gray-500">
+            In the natural flow of development, design takes center stage, and
+            web development is no different. What sets us apart in creating
+            top-notch prototypes? It starts with a focus on user convenience –
+            our UX design addresses end-users' challenges. Then, our talented UI
+            designers craft visually stunning mockups to enhance user
+            satisfaction and engagement. Finally, once the prototypes are
+            polished to perfection, we ensure they meet the customer's
+            expectations through collaborative agreement.
+          </p>
+        </li>
+        <li class="mb-10 ms-8">
+          <span
+            class="absolute flex items-center justify-center w-[45px] h-[45px] bg-blue-100 text-blue-700 rounded-full -start-6 ring-8 ring-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+              />
+            </svg>
+          </span>
+          <h3
+            class="flex items-center mb-1 text-lg font-semibold text-gray-900"
+          >
+            Website Development Stage
+          </h3>
+
+          <p class="mb-4 text-base font-normal text-gray-500">
+            At this phase, we dive into the heart of web development. This is
+            where our skilled programmers take the stage, turning prototypes
+            into a fully functional reality. For instance, let's consider a call
+            button designed by our creative team – rectangular, green, and
+            adorned with an inviting slogan ('Book tickets now'). While the
+            design is fantastic, it's the programmer's task to make it active.
+            They ensure that when a user clicks the button, they seamlessly land
+            on the coveted ticket booking page. In a nutshell, our programmers
+            translate all the creative attributes designed by the team into a
+            programming language, whether it's PHP, Java, JavaScript, Python, or
+            ASP.NET technologies.
+          </p>
+        </li>
+        <li class="mb-10 ms-8">
+          <span
+            class="absolute flex items-center justify-center w-[45px] h-[45px] bg-blue-100 text-blue-700 rounded-full -start-6 ring-8 ring-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+              />
+            </svg>
+          </span>
+          <h3
+            class="flex items-center mb-1 text-lg font-semibold text-gray-900"
+          >
+            Thorough Testing As A Guarantee of Flawless integration
+          </h3>
+
+          <p class="mb-4 text-base font-normal text-gray-500">
+            "What if a link leads to nowhere, information is jumbled, or a
+            pop-up doesn't show up? Testing often helps us reveal these flaws,
+            but no need to worry. The more bugs we uncover and fix, the smoother
+            your web solution will run. Once we're confident everything operates
+            as intended, we seamlessly integrate the web software with your
+            corporate or third-party systems, ensuring swift and flawless
+            results for your business."
+          </p>
+        </li>
+        <li class="mb-10 ms-8">
+          <span
+            class="absolute flex items-center justify-center w-[45px] h-[45px] bg-blue-100 text-blue-700 rounded-full -start-6 ring-8 ring-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+              />
+            </svg>
+          </span>
+          <h3
+            class="flex items-center mb-1 text-lg font-semibold text-gray-900"
+          >
+            Support and Maintenance
+          </h3>
+
+          <p class="mb-4 text-base font-normal text-gray-500">
+            As your campaign progresses, we'll consistently assess your website.
+            If any new issues crop up, we'll collaborate with you to develop a
+            detailed strategy for prompt fixes. Staying ahead of industry
+            guidelines, we stay abreast of the latest innovations, ensuring your
+            website remains at the forefront of SEO (search engine optimization)
+            requirements.
+          </p>
+        </li>
+      </ol>
+    </div>
+
+    <!-- End Process -->
   </div>
 </template>
 
