0b35aa008d8bf11eb68c9b5f455307b38f01cae7
shuvo-me <shuvo.js.dev@gmail.com>
2023-12-28 17:50:06 +0600

    added: buy now section

---
+++ components/Demo/ProductBuynow.vue
@@ -0,0 +1,50 @@
+<template>
+  <section
+    class="bg-[url('~/assets/images/buy-now.jpg')] bg-cover bg-no-repeat bg-center py-[5rem] mt-[4rem]"
+  >
+    <div
+      class="py-[5rem] mb-8 mx-auto max-w-7xl px-4 text-center"
+      data-aos="zoom-in"
+    >
+      <h4
+        class="mt-2 pb-16 text-3xl font-bold tracking-tight block bg-gradient-to-r from-white to-orange-400 bg-clip-text text-transparent text-gray-900 sm:text-4xl leading-10 after:bg-no-repeat after:w-full after:left-0 after:bottom-5 after:content-[url('~/assets/images/Underline.png')] after:absolute relative"
+      >
+        Get Started with <b>Product Name</b>
+      </h4>
+      <p class="text-white text-lg font-medium">
+        Boost Your Hiring Efficiency: Seize the Opportunity, Purchase
+        <b>Job Pilot</b>
+        Now!
+      </p>
+      <a
+        href="#"
+        class="border mt-12 border-purple-500 py-3 px-4 rounded-md inline-flex items-center justify-center text-white hover:bg-purple-500 transition duration-200 ease-linear gap-2"
+      >
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+        </span>
+        Buy Now
+      </a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
