a91ac5f34b4724a889a14d312dcaa1d44f0acad7
Rakib0101 <rakib.01jan@gmail.com>
2024-01-26 11:23:44 +0600

    chore: templatecookie product page static in progress

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,65 +1,37 @@
 <template>
   <div>
-    <footer
-      class="bg-gray-900 mt-20 tc-primary-footer"
-      aria-labelledby="footer-heading"
-    >
+    <footer class="bg-gray-900 mt-20 tc-primary-footer" aria-labelledby="footer-heading">
       <h2 id="footer-heading" class="sr-only">Footer</h2>
-      <div
-        class="mx-auto pt-12 px-4 sm:px-6 sm:pt-16 lg:max-w-7xl lg:px-8 lg:pt-24"
-        v-if="footer"
-      >
+      <div class="mx-auto pt-12 px-4 sm:px-6 sm:pt-16 lg:max-w-7xl lg:px-8 lg:pt-24" v-if="footer">
         <div class="xl:grid xl:grid-cols-3 xl:gap-8">
           <div class="space-y-8 xl:col-span-1">
-            <!-- <img
-              class="h-10"
-              v-if="footer.logo.url"
-              :src="footer.logo.url"
-              alt="Templatecookie Footer Logo"
-            > -->
-            <NuxtImg
-              class="h-10"
-              src="/logo-white.svg"
-              alt="Templatecookie White Logo SVG"
-            />
+            <NuxtImg class="h-10" src="/logo-white.svg" alt="Templatecookie White Logo SVG" />
             <p class="text-base text-gray-400">
               {{ footer.description }}
             </p>
             <div class="flex space-x-6" v-if="socials">
-              <a
-                :href="item.url"
-                target="_blank"
+              <a :href="item.url" target="_blank"
                 class="text-gray-400 hover:text-gray-300 h-10 w-10 flex justify-center items-center"
-                v-for="item in socials"
-                :key="item.name"
-              >
+                v-for="item in socials" :key="item.name">
                 <span class="sr-only">Facebook</span>
                 <img :src="item?.icon?.url" :alt="item.name" width="100" />
               </a>
             </div>
           </div>
-          <div
-            class="mt-12 grid grid-cols-1 md:grid-cols-2 gap-8 xl:col-span-2 xl:mt-0"
-          >
+          <div class="mt-12 grid grid-cols-1 md:grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
             <div v-for="item in footer.menuitems" :key="item.id">
               <h3 class="text-base font-medium text-gray-200">
                 {{ item.label }}
               </h3>
-              <ul role="list" class="mt-4 space-y-4" v-if="item.products">
-                <li v-for="(subItem, index) in item.products" :key="index">
-                  <nuxt-link
-                    class="text-base text-gray-400 hover:text-gray-300"
-                    :to="{ name: 'demo-slug', params: { slug: subItem.slug } }"
-                  >
-                    {{ subItem.name }}
+              <ul role="list" class="mt-4 space-y-4" v-if="selectedProducts">
+                <li v-for="(product, index) in selectedProducts" :key="index">
+                  <nuxt-link class="text-base text-gray-400 hover:text-gray-300" :to="product.slug">
+                    {{ product.name }}
                   </nuxt-link>
                 </li>
 
                 <li>
-                  <nuxt-link
-                    to="/products"
-                    class="text-base text-gray-400 group hover:text-gray-100"
-                  >
+                  <nuxt-link to="/products" class="text-base text-gray-400 group hover:text-gray-100">
                     Free HTML & Figma Templates
                   </nuxt-link>
                 </li>
@@ -69,124 +41,78 @@
               <h3 class="text-base font-medium text-gray-200">Get More Done</h3>
               <ul role="list" class="mt-4 space-y-4">
                 <li>
-                  <nuxt-link
-                    to="/elite-support"
-                    class="text-base text-gray-400 group hover:text-gray-100"
-                  >
+                  <nuxt-link to="/elite-support" class="text-base text-gray-400 group hover:text-gray-100">
                     <div>Elite Support</div>
-                    <span
-                      class="text-xs text-gray-400 group-hover:text-gray-200"
-                    >
+                    <span class="text-xs text-gray-400 group-hover:text-gray-200">
                       Elevate Your Success with Premium Assistance
                     </span>
                   </nuxt-link>
                 </li>
                 <li>
-                  <nuxt-link
-                    to="/hire-us"
-                    class="text-base text-gray-400 group hover:text-gray-100"
-                  >
+                  <nuxt-link to="/hire-us" class="text-base text-gray-400 group hover:text-gray-100">
                     <div>Hire Development Team</div>
-                    <span
-                      class="text-xs text-gray-400 group-hover:text-gray-200"
-                      >Transform Your Vision into Reality with Our
-                      Expertise.</span
-                    >
+                    <span class="text-xs text-gray-400 group-hover:text-gray-200">Transform Your Vision into Reality
+                      with Our
+                      Expertise.</span>
                   </nuxt-link>
                 </li>
 
                 <li>
-                  <nuxt-link
-                    to="/installation-plans"
-                    class="text-base text-gray-400 group hover:text-gray-100"
-                  >
+                  <nuxt-link to="/installation-plans" class="text-base text-gray-400 group hover:text-gray-100">
                     <div>Installation Support</div>
                     <!-- <span class="text-xs text-gray-400 group-hover:text-gray-200">Seamless Setup for a Worry-Free Start</span> -->
-                    <span
-                      class="text-xs text-gray-400 group-hover:text-gray-200"
-                      >Seamlessly Launch Your Projects with Professional
-                      Setup.</span
-                    >
+                    <span class="text-xs text-gray-400 group-hover:text-gray-200">Seamlessly Launch Your Projects with
+                      Professional
+                      Setup.</span>
                   </nuxt-link>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div
-          class="mt-12 border-t border-white/10 pt-8 sm:mt-16 lg:mt-16 lg:flex lg:items-center lg:justify-center"
-        >
-          <nav
-            class="-mb-6 columns-2 sm:flex flex-wrap xl:flex-nowrap gap-x-6 lg:gap-x-0 lg:justify-between w-full"
-            aria-label="Footer"
-          >
+        <div class="mt-12 border-t border-white/10 pt-8 sm:mt-16 lg:mt-16 lg:flex lg:items-center lg:justify-center">
+          <nav class="-mb-6 columns-2 sm:flex flex-wrap xl:flex-nowrap gap-x-6 lg:gap-x-0 lg:justify-between w-full"
+            aria-label="Footer">
             <div class="pb-2">
-              <nuxt-link
-                to="/get-support"
-                class="text-sm leading-6 text-gray-400 hover:text-gray-100"
-              >
+              <nuxt-link to="/get-support" class="text-sm leading-6 text-gray-400 hover:text-gray-100">
                 Create Support Ticket
               </nuxt-link>
             </div>
             <div class="pb-2">
-              <nuxt-link
-                to="/docs"
-                class="text-sm leading-6 text-gray-400 hover:text-gray-100"
-              >
+              <nuxt-link to="/docs" class="text-sm leading-6 text-gray-400 hover:text-gray-100">
                 Documentation
               </nuxt-link>
             </div>
             <div class="pb-2">
-              <a
-                href="https://1.envato.market/EaNJ2X"
-                target="_blank"
-                class="text-sm leading-6 text-gray-400 hover:text-gray-100"
-                >Codecanyon Profile</a
-              >
+              <a href="https://1.envato.market/EaNJ2X" target="_blank"
+                class="text-sm leading-6 text-gray-400 hover:text-gray-100">Codecanyon Profile</a>
             </div>
             <div class="pb-2">
-              <nuxt-link
-                to="/blog"
-                class="text-sm leading-6 text-gray-400 hover:text-gray-100"
-              >
+              <nuxt-link to="/blog" class="text-sm leading-6 text-gray-400 hover:text-gray-100">
                 Read Our Blog
               </nuxt-link>
             </div>
             <div class="pb-2">
-              <nuxt-link
-                to="/privacy-policy"
-                class="text-sm leading-6 text-gray-400 hover:text-gray-100"
-              >
+              <nuxt-link to="/privacy-policy" class="text-sm leading-6 text-gray-400 hover:text-gray-100">
                 Privacy Policy
               </nuxt-link>
             </div>
             <div class="pb-2">
-              <nuxt-link
-                to="/refund-policy"
-                class="text-sm leading-6 text-gray-400 hover:text-gray-100"
-              >
+              <nuxt-link to="/refund-policy" class="text-sm leading-6 text-gray-400 hover:text-gray-100">
                 Refund Policy
               </nuxt-link>
             </div>
             <div class="pb-2">
-              <nuxt-link
-                to="/terms"
-                class="text-sm leading-6 text-gray-400 hover:text-gray-100"
-              >
+              <nuxt-link to="/terms" class="text-sm leading-6 text-gray-400 hover:text-gray-100">
                 Terms & Condition
               </nuxt-link>
             </div>
           </nav>
         </div>
-        <div
-          class="mt-12 border-t border-white/10 py-6 md:flex md:items-center md:justify-between"
-        >
+        <div class="mt-12 border-t border-white/10 py-6 md:flex md:items-center md:justify-between">
           <div class="md:order-2">
-            <NuxtImg
-              class="h-[37px] w-[390px]"
-              src="/images/stripe-payment.svg"
-              alt="Templatecookie Payment Gateway Images"
-            />
+            <NuxtImg class="h-[37px] w-[390px]" src="/images/stripe-payment.svg"
+              alt="Templatecookie Payment Gateway Images" />
           </div>
           <p class="mt-8 text-sm text-gray-400 md:order-3 md:mt-0">
             &copy; {{ new Date().getFullYear() }} All Rights Reserved -
@@ -199,10 +125,40 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-const { data, social } = defineProps(["data", "social"]);
-const footer = ref([]);
-const socials = ref([]);
-footer.value = data;
-socials.value = social;
+  import { ref } from "vue";
+  const { data, social } = defineProps(["data", "social"]);
+  const footer = ref([]);
+  const socials = ref([]);
+  footer.value = data;
+  socials.value = social;
+  const selectedProducts =
+    [
+      {
+        "name": "Job Portal Script: Maximize Your Earning Potential with Jobpilot",
+        "slug": "demo/jobpilot-job-portal-script-php-laravel",
+        "shortName": "Jobpilot",
+        "thumbnail": {
+          "url": "https://www.datocms-assets.com/73511/1669616355-jobpilot.png"
+        },
+        "description": "Jobpilot is a  modern and high-quality Job Portal Script that offers a wealth of features and tools to help you create a successful job portal website."
+      },
+      {
+        "name": "Schooling - The Ultimate PHP Script for Efficient School Management",
+        "slug": "demo/schooling-school-management-system-script",
+        "shortName": "Schooling",
+        "thumbnail": {
+          "url": "https://www.datocms-assets.com/73511/1669616359-schooling.png"
+        },
+        "description": "Efficiently manage your school with Schooling. The all-in-one PHP script for schools with multi-language support, payment gateway integration, and powerful academic management features. Try it now."
+      },
+      {
+        "name": "Adlisting - Classified Ads PHP Script",
+        "slug": "demo/adlisting-classified-ads-script",
+        "shortName": "Adlisting",
+        "thumbnail": {
+          "url": "https://www.datocms-assets.com/73511/1671949568-adlisting.jpg"
+        },
+        "description": "Adlisting is a premium PHP script with minimal, clean, flexible, and structured code. Using the script you can create stunning ad listing websites to buy/sell classified ads and generate revenues."
+      }
+    ]
 </script>
---
+++ pages/demo/jobpilot-job-portal-script-php-laravel.vue
@@ -0,0 +1,434 @@
+<template>
+  <div>
+    <demo-header :product="product" v-if="product" />
+    <demo-product-hero :product="product" v-if="product" />
+    <demo-why-choose-our-product :product="product" v-if="product?.whyChooseUs" />
+    <div v-for="(section, index) in product?.contents" :key="index">
+      <div v-if="section.__typename == 'ExclusivefeatureRecord'">
+        <demo-exclusive-feature :data="section" />
+      </div>
+      <div v-if="section.__typename == 'FeaturescreenshotRecord'">
+        <demo-product-screenshots :data="section" />
+      </div>
+      <div v-if="section.__typename == 'TopfeatureRecord'">
+        <demo-product-top-features :data="section" />
+      </div>
+      <div v-if="section.__typename == 'ProductctaRecord'">
+        <demo-call-to-action :section="section" :product="product" />
+      </div>
+      <div v-if="section.__typename == 'PriceplanRecord'">
+        <PricingSection :plans="section.plans" :id="section.sectionId ? section.sectionId : section.id"
+          :info="section.info[0]" :extra-offer="product.extraOffer"
+          :request-customization="product.offerRequestCustomization" />
+      </div>
+      <div v-if="section.__typename == 'TestimonialSectionRecord'">
+        <demo-testimonial-section :data="section" />
+      </div>
+      <div v-if="section.__typename == 'TechnologySectionRecord'">
+        <demo-technology-section :data="section" :product="product" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+  definePageMeta({
+    layout: "empty",
+  });
+  const product = {
+    "id": "128748729",
+    "name": "Job Portal Script: Maximize Your Earning Potential with Jobpilot",
+    "description": "Jobpilot is a modern and high-quality Job Portal Script that offers a wealth of features and tools to help you create a successful job portal website.",
+    "docsUrl": "/docs/jobpilot/",
+    "previewUrl": "https://jobpilot.templatecookie.com/",
+    "shortName": "Jobpilot",
+    "offerRequestCustomization": true,
+    "whyChooseUs": true,
+    "banner": {
+      "url": "https://www.datocms-assets.com/73511/1690592181-preview-image-1.png"
+    },
+    "demoBanner": {
+      "url": "https://www.datocms-assets.com/73511/1680017497-shikkha-1.png"
+    },
+    "logoCloudsTitle": "6 Different Payment Gateways Integrated",
+    "logoCloudsImages": [
+      {
+        "url": "https://www.datocms-assets.com/73511/1673600263-paypal.svg",
+        "alt": null
+      },
+      {
+        "url": "https://www.datocms-assets.com/73511/1673600269-stripe.svg",
+        "alt": null
+      },
+      {
+        "url": "https://www.datocms-assets.com/73511/1673600364-flutterwave.png",
+        "alt": null
+      },
+      {
+        "url": "https://www.datocms-assets.com/73511/1673600266-razorpay_logo.svg",
+        "alt": null
+      },
+      {
+        "url": "https://www.datocms-assets.com/73511/1673600256-midtrans.svg",
+        "alt": null
+      },
+      {
+        "url": "https://www.datocms-assets.com/73511/1673600375-sslcommerz.png",
+        "alt": null
+      }
+    ],
+    "seo": null,
+    "showNotice": false,
+    "noticeBar": {
+      "blocks": [],
+      "links": [],
+      "value": {
+        "schema": "dast",
+        "document": {
+          "type": "root",
+          "children": [
+            {
+              "type": "paragraph",
+              "children": [
+                {
+                  "type": "span",
+                  "value": ""
+                }
+              ]
+            }
+          ]
+        }
+      }
+    },
+    "actionButtons": [
+      {
+        "label": "Try Live Demo",
+        "href": "https://jobpilot.templatecookie.com/",
+        "target": "_blank",
+        "cssClass": ""
+      },
+      {
+        "label": "View Pricing",
+        "href": "#pricing",
+        "target": "",
+        "cssClass": ""
+      }
+    ],
+    "menuItems": [
+      {
+        "label": "Top Features",
+        "id": "133264624",
+        "href": "#features"
+      },
+      {
+        "label": "Pricing",
+        "id": "133264625",
+        "href": "#pricing"
+      },
+      {
+        "label": "Testimonials",
+        "id": "147461428",
+        "href": "#testimonials"
+      }
+    ],
+    "extraOffer": [],
+    "contents": [
+      {
+        "__typename": "TopfeatureRecord",
+        "id": "133264538",
+        "sectionId": "features",
+        "info": [
+          {
+            "title": "Top Features of Schooling",
+            "description": "",
+            "bgClass": "",
+            "background": null
+          }
+        ],
+        "features": [
+          {
+            "title": "Multi Language Supported",
+            "description": "Ability to switch between multiple languages for ease of use by different users.",
+            "cssClass": "",
+            "id": "133264573",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1654049751-2-multiple-language.svg"
+            }
+          },
+          {
+            "title": "Multi Currency",
+            "description": "Support for multiple currencies to facilitate payments from different regions.",
+            "cssClass": "",
+            "id": "133264574",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1653558189-money.svg"
+            }
+          },
+          {
+            "title": "6 Different Payment Gateways",
+            "description": " Integration with six different payment gateway options for convenient transactions.",
+            "cssClass": "",
+            "id": "133264575",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1653534552-creditcard.svg"
+            }
+          },
+          {
+            "title": "Recurring Admission Plan",
+            "description": "Option for recurring admission payment plans for parents and students.",
+            "cssClass": "",
+            "id": "133264576",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1653558194-updates.svg"
+            }
+          },
+          {
+            "title": "One time & Recurring Invoice",
+            "description": " Creation of one-time and recurring invoices to streamline payment management.",
+            "cssClass": "",
+            "id": "133264577",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1654049754-3-multiple-payment-gateway.svg"
+            }
+          },
+          {
+            "title": "5 User Types",
+            "description": " Different user types (admin, teacher, student, parent, accountant) for distinct access and functionality.",
+            "cssClass": "",
+            "id": "133264578",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1654065392-users.svg"
+            }
+          },
+          {
+            "title": "Full Academic Management",
+            "description": "Comprehensive management of academic activities, including scheduling, grading, and attendance.",
+            "cssClass": "",
+            "id": "133264579",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059490-student-education.svg"
+            }
+          },
+          {
+            "title": "Student Result Card",
+            "description": "Creation and access of student result cards for academic progress tracking.",
+            "cssClass": "",
+            "id": "133264580",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059584-certificate-card.svg"
+            }
+          },
+          {
+            "title": "Live Class Schedule",
+            "description": "Display of live class schedule for students and teachers.",
+            "cssClass": "",
+            "id": "133264581",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1654065098-calendar.svg"
+            }
+          },
+          {
+            "title": "Online Class with Zoom",
+            "description": " Conducting online classes with Zoom integration for remote learning.",
+            "cssClass": "",
+            "id": "133264582",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059656-video-play.svg"
+            }
+          },
+          {
+            "title": "Student & Teacher Group Chat",
+            "description": " Facilitation of group chat between students and teachers for communication and collaboration",
+            "cssClass": "",
+            "id": "133264583",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059815-group-chat.svg"
+            }
+          },
+          {
+            "title": "Assignment with File Sharing",
+            "description": "Assignment submission and file sharing between teachers and students.",
+            "cssClass": "",
+            "id": "133264584",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059820-notebook-assignment.svg"
+            }
+          },
+          {
+            "title": "PDF & Excel Reports",
+            "description": "Generation of reports in PDF and Excel formats for easy data analysis and record-keeping.",
+            "cssClass": "",
+            "id": "133264585",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059851-file-xls-duotone.svg"
+            }
+          },
+          {
+            "title": "Online Student Admission",
+            "description": "Online admission process for new students to streamline enrollment.",
+            "cssClass": "",
+            "id": "133264586",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059954-user-circle-plus-duotone.svg"
+            }
+          },
+          {
+            "title": "Dark Mode Supported",
+            "description": "Option to switch to dark mode for a more comfortable and less eye-straining user interface.",
+            "cssClass": "",
+            "id": "133264587",
+            "image": {
+              "url": "https://www.datocms-assets.com/73511/1680059818-moon-stars-dark-mode.svg"
+            }
+          }
+        ],
+        "cssClass": ""
+      },
+      {
+        "__typename": "TestimonialSectionRecord",
+        "id": "147461427",
+        "title": "Testimonials",
+        "sectionId": "testimonials",
+        "description": "Don't just listen to our words, see what our valued customers think about our product scripts and templates.\n",
+        "testimonials": [
+          {
+            "id": "90914894",
+            "name": "by Brandego_Media",
+            "role": "From Codecanyon Market",
+            "quote": "The code is well organized and the application is lightweight. Customer support is also very good. I would recommend this purchase!",
+            "image": null
+          },
+          {
+            "id": "90914909",
+            "name": "by rks784262",
+            "role": "From Codecanyon Market",
+            "quote": "This script is a true masterpiece. Exceptionally good design and UI. Plenty of features for a reasonable price. Great value for money. The support team is also responsive. Keep improving. Thank you",
+            "image": null
+          },
+          {
+            "id": "90914865",
+            "name": "by nursahal00 ",
+            "role": "From Codecanyon Market",
+            "quote": "templatecookie is awesome, has great customer service, is a knowledgeable and great personality. Thank you for the amazing support!",
+            "image": null
+          }
+        ]
+      },
+      {
+        "__typename": "PriceplanRecord",
+        "id": "147461435",
+        "sectionId": "pricing",
+        "info": [
+          {
+            "id": "147461432",
+            "title": "High Quality Product, Affordable Pricing",
+            "description": "We develop high-quality products using modern technologies but still keep the pricing very reasonable. That way we can help more and more people create their dream projects!",
+            "createdAt": "2023-04-29T08:42:35+06:00",
+            "bgClass": ""
+          }
+        ],
+        "plans": [
+          {
+            "name": "Extended",
+            "description": "Extended license permits you to ask and make money from your users. Sourcecode included. This license permits you for 1 domain, purchase more license if you have multiple sites. ",
+            "price": 149,
+            "purchaseLink": "https://go.templatecookie.com/schooling-extended",
+            "paddleProductId": "",
+            "paddleCheckout": false,
+            "features": []
+          },
+          {
+            "name": "Regular",
+            "description": "Regular licenses allow you to let users use the platform free of charge. Sourcecode included. This license permits you to have 1 domain; purchase more licenses if you have multiple sites.",
+            "price": 59,
+            "purchaseLink": "https://go.templatecookie.com/schooling-regular",
+            "paddleProductId": "",
+            "paddleCheckout": false,
+            "features": []
+          }
+        ]
+      },
+      {
+        "__typename": "ProductctaRecord",
+        "id": "133556679",
+        "title": "Build Your Awesome School Management System Today!",
+        "description": "",
+        "cssClass": "",
+        "screenshots": []
+      }
+    ]
+  };
+  const route = useRoute();
+  const { slug } = route?.params;
+  const title = product?.seo?.title ?? product?.name;
+  const description =
+    product?.seo?.description ?? product?.description;
+  const image = product?.banner?.url ?? data?.image;
+
+  useHead({
+    link: [
+      {
+        rel: "canonical",
+        href: "https://templatecookie.com" + route.path,
+      },
+    ],
+  });
+
+  useSeoMeta({
+    title: title,
+    ogTitle: title,
+    description: description,
+    ogDescription: description,
+    ogImage: image,
+  });
+
+  defineOgImage({
+    title: title,
+    description: description,
+    component: "Projects",
+  });
+</script>
+
+<style lang="scss" scoped>
+  /* animation */
+  .slider-full {
+    animation: scroll 26s linear infinite;
+  }
+
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
+    }
+
+    100% {
+      transform: translateX(calc(-250px * 7));
+    }
+  }
+
+  /* swiper single silder */
+  .single-slider {
+
+    .swiper-button-prev,
+    .swiper-button-next {
+      top: calc(50% - 15px);
+      transform: translateY(-50%);
+    }
+
+    .swiper-button-prev:after,
+    .swiper-button-next:after {
+      content: none;
+    }
+  }
+
+  .tab-content-wrap {
+    &.active {
+      background-color: #0b63e5;
+
+      h3,
+      p {
+        color: white;
+      }
+    }
+  }
+</style>
