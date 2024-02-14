69b83520394deaafb0db7b836510ef4e07ea275f
shuvo-me <shuvo.js.dev@gmail.com>
2024-02-14 10:46:45 +0600

    feat: add free and latest product tab on home page

--- components/ProductSection.vue
+++ components/ProductSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="latest" v-if="homepage && homepage?.latestProduct" id="products">
-    <div class="mx-auto max-w-7xl py-20 sm:py-24 lg:py-32 px-4 sm:px-6 lg:px-8">
-      <div class="mb-[72px] text-center">
+    <div class="mx-auto max-w-7xl py-20 sm:py-24 lg:py-32 px-4 sm:px-6 lg:px-8 space-y-6">
+      <div class="text-center">
         <h2 class="text-4xl md:text-5xl text-dark font-semibold capitalize tracking-1 mb-8">
           {{ homepage.latestProduct[0].title }}
         </h2>
@@ -9,17 +9,40 @@
           {{ homepage.latestProduct[0].description }}
         </p>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div class=" flex justify-center">
+        <RadioGroup v-model="tab"
+          class="grid grid-cols-2 gap-x-1 rounded-full p-1 text-center text-xs font-semibold leading-5 ring-1 ring-inset ring-gray-200">
+          <RadioGroupOption as="template" v-for="option in tabs" :key="option.value" :value="option" v-slot="{ checked }">
+            <div
+              :class="[checked ? 'bg-blue-600 text-white' : 'text-gray-500', 'cursor-pointer rounded-full px-2.5 py-1']"
+              class="text-base">
+              <span>{{ option.label }}</span>
+            </div>
+          </RadioGroupOption>
+        </RadioGroup>
+      </div>
+      <free-products v-if="tab.value === 'free'" />
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6" v-if="tab.value === 'latest'">
         <div v-for="(item, index) in latestProducts" :key="index" class="flex items-stretch aos" data-aos="fade-up"
           :data-aos-delay="index * 100">
           <ProductCard :product="item" :large="false" />
         </div>
       </div>
+      <div class="flex items-center justify-center w-full">
+        <nuxt-link href="/products"
+          class="mt-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-secondary">View
+          All </nuxt-link>
+      </div>
     </div>
+
   </section>
 </template>
 
 <script setup>
+
+import { ref } from 'vue'
+import { RadioGroup, RadioGroupOption } from '@headlessui/vue'
+
 defineProps({
   homepage: {
     type: Object,
@@ -30,4 +53,14 @@ defineProps({
     required: true
   }
 })
+
+const tabs = [
+  { value: 'free', label: 'Free' },
+  { value: 'latest', label: 'Latest' },
+]
+
+
+const tab = ref(tabs[0])
+
+
 </script>
