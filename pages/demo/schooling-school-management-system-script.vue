5fa7aa587f8cceaf14946c80caeff30ef9fefca6
shuvo-me <shuvo.js.dev@gmail.com>
2024-02-15 12:08:10 +0600

    fix: schooling  pirce float number issue

--- pages/demo/schooling-school-management-system-script.vue
+++ pages/demo/schooling-school-management-system-script.vue
@@ -337,7 +337,7 @@ const product = {
         {
           "name": "Extended",
           "description": "Extended license permits you to ask and make money from your users. Sourcecode included. This license permits you for 1 domain, purchase more license if you have multiple sites. ",
-          "price": productPrice?.value[1]?.price,
+          "price": parseInt(productPrice?.value[1]?.price),
           "purchaseLink": "https://go.templatecookie.com/schooling-extended",
           "paddleProductId": "",
           "paddleCheckout": false,
@@ -346,7 +346,7 @@ const product = {
         {
           "name": "Regular",
           "description": "Regular licenses allow you to let users use the platform free of charge. Sourcecode included. This license permits you to have 1 domain; purchase more licenses if you have multiple sites.",
-          "price": productPrice?.value[0]?.price,
+          "price": parseInt(productPrice?.value[0]?.price),
           "purchaseLink": "https://go.templatecookie.com/schooling-regular",
           "paddleProductId": "",
           "paddleCheckout": false,
