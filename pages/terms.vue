8b63f23dc995a48ab454a798c3ce78763a7794b9
shuvo-me <shuvo.js.dev@gmail.com>
2024-02-20 15:16:19 +0600

    fix: canonical link in terms page

--- pages/terms.vue
+++ pages/terms.vue
@@ -101,7 +101,7 @@ useHead({
   link: [
     {
       rel: "canonical",
-      href: "https://templatecookie.com" + route.path,
+      href: "https://www.templatecookie.com" + route.path,
     },
   ],
 });
