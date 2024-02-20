7d9d2fbf3c849c09f6d952f7a1bd0a71859d77c8
shuvo-me <shuvo.js.dev@gmail.com>
2024-02-20 11:49:19 +0600

    fix: img tab alt attr missing in schooling page

--- pages/demo/schooling-school-management-system-script.vue
+++ pages/demo/schooling-school-management-system-script.vue
@@ -55,28 +55,36 @@ const product = {
   "logoCloudsTitle": "6 Different Payment Gateways Integrated",
   "logoCloudsImages": [
     {
+      "url": "https://www.datocms-assets.com/73511/1673600256-midtrans.svg",
+      "alt": 'midtrans'
+    },
+    {
+      "url": "https://www.datocms-assets.com/73511/1673600260-mollie-1.svg",
+      "alt": 'mollie'
+    },
+    {
       "url": "https://www.datocms-assets.com/73511/1673600263-paypal.svg",
-      "alt": null
-    },
-    {
-      "url": "https://www.datocms-assets.com/73511/1673600269-stripe.svg",
-      "alt": null
-    },
-    {
-      "url": "https://www.datocms-assets.com/73511/1673600364-flutterwave.png",
-      "alt": null
+      "alt": "paypal"
     },
     {
       "url": "https://www.datocms-assets.com/73511/1673600266-razorpay_logo.svg",
-      "alt": null
+      "alt": "razorpay"
     },
     {
-      "url": "https://www.datocms-assets.com/73511/1673600256-midtrans.svg",
-      "alt": null
+      "url": "https://www.datocms-assets.com/73511/1673600269-stripe.svg",
+      "alt": "stripe"
+    },
+    {
+      "url": "https://www.datocms-assets.com/73511/1673600364-flutterwave.png",
+      "alt": "flutterwave"
+    },
+    {
+      "url": "https://www.datocms-assets.com/73511/1673600370-instamojo.png",
+      "alt": "instamojo"
     },
     {
       "url": "https://www.datocms-assets.com/73511/1673600375-sslcommerz.png",
-      "alt": null
+      "alt": "sslcommerz"
     }
   ],
   "seo": {
